c05f6fbf9ac21e996569ca6b5ad42882a4bb39ea
Ann <annkarokiadmissions@gmail.com>
2025-12-19 20:20:41 +0300

    feat: add landing page

---
+++ my-portfolio/src/Components/LandingPage.jsx
@@ -0,0 +1,26 @@
+function LandingPage() {
+    return (
+        <div className="landing-page">
+            <img src="#" className="hero-img"></img>
+            <h1>Hi, I am Ann Charity</h1>
+            <p>I am the Integrated Full-Cycle Creator</p>
+
+            <div className="social-links">
+                <a href="#">
+                    <img src="#"></img>
+                </a>
+
+                <a href="#">
+                    <img src="#"></img>
+                </a>
+
+                <a href="#">
+                    <img src="#"></img>
+                </a>
+
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
