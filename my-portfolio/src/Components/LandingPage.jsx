4e5f096721d81f482aacc836c94f337ad2868d8e
Ann <annkarokiadmissions@gmail.com>
2025-12-19 21:26:52 +0300

    style: add background and text

--- my-portfolio/src/Components/LandingPage.jsx
+++ my-portfolio/src/Components/LandingPage.jsx
@@ -1,21 +1,22 @@
+import { FaInstagram, FaTwitter, FaLinkedin} from 'react-icons/fa'
+
 function LandingPage() {
     return (
         <div className="landing-page">
-            <img src="#" className="hero-img"></img>
             <h1>Hi, I am Ann Charity</h1>
             <p>I am the Integrated Full-Cycle Creator</p>
 
             <div className="social-links">
                 <a href="#">
-                    <img src="#"></img>
+                    <FaInstagram size={30}/>
                 </a>
 
                 <a href="#">
-                    <img src="#"></img>
+                    <FaTwitter size={30}/>
                 </a>
 
                 <a href="#">
-                    <img src="#"></img>
+                    <FaLinkedin size={30}/>
                 </a>
 
             </div>
